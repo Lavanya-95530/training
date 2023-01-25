6cad30968c8700046f9beb252b6b58b3d3129a1b
Lavanya-95530 <lavanya.donthala@feuji.com>
2023-01-25 15:26:22 +0530

    SPA

--- single-page-application/spa.js
+++ single-page-application/spa.js
@@ -45,5 +45,8 @@ window.onpopstate = () => {
 mainDiv.innerHTML = Match[window.location.pathname];
 };
 
-
+// const routes = 
+// {    404: "404",    "/": "pushHome.html",    "/about": "pushAbout.html",    "/contact": 
+// "pushContact.html",};const handleLocation = async () => {    const path = window.location.pathname;    const route = routes[path] || routes[404];    const html = await fetch(route).then((data) => data.text());    
+//   document.getElementById("main-page").innerHTML = html;};
 
